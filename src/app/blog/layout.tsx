70f211862f071c958ce983f13881ef8524640c19
kaka <2833055765@qq.com>
2026-07-06 23:57:39 +0800

    feat: 添加 blog loading 组件和异步数据获取，更新 layout 标题

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -8,7 +8,7 @@ export default function BlogLayout({
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Blog 布局组件?</h1>
+      <h1>Blog layout</h1>
       <button
         onClick={() => {
           setCount(count + 1);
